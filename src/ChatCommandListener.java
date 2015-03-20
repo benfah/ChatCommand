4f61c953ff47e5dc3c4fe03501f1964bded799ec
benfah <vayegame@gmail.com>
2015-03-20 15:21:46 +0100

    Create ChatCommandListener.java

---
+++ src/ChatCommandListener.java
@@ -0,0 +1,17 @@
+package me.benfah.manager;
+
+import org.bukkit.entity.Player;
+
+public interface ChatCommandListener {
+
+	
+	
+
+	public abstract void onChatCommand(Player sender, String[] cmd);
+	
+	
+	
+	
+	
+	
+}
